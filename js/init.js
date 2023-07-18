a0166e2777b23a7ebe340d6fe7165a41cb56d50a
formaaaol0313 <formaaaol0313@gmail.com>
2023-07-17 20:09:58 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDwoalL3QLTRvBP26XNtuP3nw5_j9mcTR8",
+  authDomain: "alumnoaaol.firebaseapp.com",
+  projectId: "alumnoaaol",
+  storageBucket: "alumnoaaol.appspot.com",
+  messagingSenderId: "809984202125",
+  appId: "1:809984202125:web:d29132189efc73210699c4",
+  measurementId: "G-8MVQSQ3HY3"
+});
